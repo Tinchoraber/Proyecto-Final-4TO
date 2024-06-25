892fba42de1284b4a9bdd59f877146b95a42ab0b
marcos <marcosmellebovsky@gmail.com>
2024-06-24 23:25:05 -0300

    mejora de tus reservas y mejora de eliminar reserva

--- obj/Debug/net7.0/ProyectoFinal4To.AssemblyInfo.cs
+++ obj/Debug/net7.0/ProyectoFinal4To.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoFinal4To")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f65801325a3708c4af529df3b58e45aba4c7166d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoFinal4To")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoFinal4To")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
